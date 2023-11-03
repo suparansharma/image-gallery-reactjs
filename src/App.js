24148cb2d4b617356542e1d34ceea9142941750a
Suparan Sharma <suparansharma@gmail.com>
2023-11-03 11:25:32 -0700

    drag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 import { getImages } from './api';
 
 function App() {
   const [images, setImages] = useState([]);
   const [checkboxValues, setCheckboxValues] = useState({});
-  const[selectedImages,setSelectedImages]= useState([]);
-  console.log("checkboxValues",checkboxValues);
-  console.log("selectedImages",selectedImages);
+  const [selectedImages, setSelectedImages] = useState([]);
+  console.log("checkboxValues", checkboxValues);
+  console.log("selectedImages", selectedImages);
 
   useEffect(() => {
     getImages().then((data) => {
@@ -48,30 +49,53 @@ function App() {
     setCheckboxValues(updatedCheckboxValues);
   };
 
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(images);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setImages(items);
+  }
+
   return (
-<div className="container">
-  <button onClick={handleDelete}>Delete Selected Images</button>
-  <p>Selected Images Count: {selectedImages.length}</p>
-  <div className="row">
-    {images.map((image) => (
-      <div className="col-4 custom-column" key={image.id}> 
-        <div className="card card-hover custom-card">
-          <div className="position-relative">
-            <input
-              className="form-check-input position-absolute top-0 start-0 m-3"
-              type="checkbox"
-              id={`inlineCheckbox${image.id}`}
-              name={image.id}
-              checked={checkboxValues[image.id]}
-              onChange={(event) => handleCheckboxChange(event, image.id)}
-            />
-          </div>
-          <img src={process.env.PUBLIC_URL + `/images/${image.image}`} alt={image.name} className="card-img-top" />
-        </div>
-      </div>
-    ))}
-  </div>
-</div>
+    <div className="container">
+      <button onClick={handleDelete}>Delete Selected Images</button>
+      <p>Selected Images Count: {selectedImages.length}</p>
+
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="characters">
+          {(provided) => (
+            <div className="row">
+              {images.map(({ image, id, name }, index) => (
+                <Draggable key={id} draggableId={id} index={index}>
+                  {(provided) => (
+                    <div className="col-4 custom-column" key={id}>
+                      <div className="card card-hover custom-card">
+                        <div className="position-relative">
+                          <input
+                            className="form-check-input position-absolute top-0 start-0 m-3"
+                            type="checkbox"
+                            id={`inlineCheckbox${id}`}
+                            name={image.id}
+                            checked={checkboxValues[id]}
+                            onChange={(event) => handleCheckboxChange(event, id)}
+                          />
+                        </div>
+                        <img src={process.env.PUBLIC_URL + `/images/${image}`} alt={name} className="card-img-top" />
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+
+            </div>
+
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
 
 
 
